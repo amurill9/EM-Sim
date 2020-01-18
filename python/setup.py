26b48d6dac057b9866ccafdde0341b91aaeaa641
Andrew Dinh <github@andrewkdinh.com>
2020-01-18 15:00:55 -0800

    Update setup.py
    Also added .gitignore

--- python/setup.py
+++ python/setup.py
@@ -8,15 +8,22 @@ setuptools.setup(
     version="0.0.2",
     author="EM.py-r3-CAL()",
     # author_email="",
-    description="Electromagnetic Simulator",
+    description="EM-Sim: Electromagnetic Simulator",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/amurill9/EM-Sim",
+    platforms=['Windows', 'Linux', 'Mac OS-X'],
     packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy'
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+        'Natural Language :: English',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics',
     ],
     python_requires='>=3.6',
 )
