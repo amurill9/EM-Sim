252bb834ececabe64b0ec9ec049225507ab52394
daveslh <60028526+daveslh@users.noreply.github.com>
2020-01-18 14:38:49 -0800

    Update em_sim.h

--- em_sim.h
+++ em_sim.h
@@ -1,7 +1,11 @@
 #include <math.h>
+#include <stdlib.h>
 
-float gaussE(float epsilon, float rho, float omega);
+float gaussE(float epsilon, float rho, omega o);
 
-float gaussRho(float epsilon, float E, float omega);
+float gaussRho(float epsilon, float E, omega o);
 
-float gaussEpsilon(float E, float rho, float omega);
+float gaussEpsilon(float E, float rho, omega o);
+
+typedef struct omega {float rho, float theta, float phi} omega;
+
